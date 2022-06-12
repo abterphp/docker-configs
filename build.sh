a6a0f67eb8654336d784f7940edd667fc2168833
Peter Aba <zsolt.peter.aba@zalando.de>
2022-06-12 22:16:22 +0200

    Add php 8.1

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 cd "$(dirname "$0")" || exit
 
-docker pull php:8.0-fpm-alpine
+docker pull php:8.0-fpm-alpine php:8.1-fpm-alpine
 
-docker build -t abtercms/php:80 -t abtercms/php:latest -f php/Dockerfile-80 php
+docker build -t abtercms/php:80 -f php/Dockerfile-80 php
+docker build -t abtercms/php:81 -t abtercms/php:latest -f php/Dockerfile-81 php
 
 docker push abtercms/php:80
+docker push abtercms/php:81
 docker push abtercms/php:latest
