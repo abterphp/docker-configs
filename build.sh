c5811b21c7178cac10ff0dfcf401b2e46a44b62a
Peter Aba <zsolt.peter.aba@zalando.de>
2023-01-23 00:06:57 +0100

    Upgrade to php 8.2

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "$0")" || exit
 
-docker pull php:8.0-fpm-alpine php:8.1-fpm-alpine
+docker pull php:8.1-fpm-alpine php:8.2-fpm-alpine
 
-docker build -t abtercms/php:80 -f php/Dockerfile-80 php
-docker build -t abtercms/php:81 -t abtercms/php:latest -f php/Dockerfile-81 php
+docker build -t abtercms/php:81 -f php/Dockerfile-81 php
+docker build -t abtercms/php:82 -t abtercms/php:latest -f php/Dockerfile-82 php
 
-docker push abtercms/php:80
 docker push abtercms/php:81
+docker push abtercms/php:82
 docker push abtercms/php:latest
